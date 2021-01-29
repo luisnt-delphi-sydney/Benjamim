1620a163c2cf4e4f465328796fb2735561f0d1af
setsl.luisnt.delphi.sydney@gmail.com <setsl-luisnt-delphi-sydney>
2021-01-29 16:44:53 -0300

    Sample Update

--- sample/BenjamimTest.dpr
+++ sample/BenjamimTest.dpr
@@ -7,17 +7,38 @@ program BenjamimTest;
 
 uses
   System.SysUtils,
-  Benjamim.JWT;
+  Benjamim;
+
+var
+  LToken: string;
 
 begin
   ReportMemoryLeaksOnShutdown := true;
 
   try
-    Benjamim.JWT;
+    Benjamim.JWT.Password('secret'); { OPTIONAL }
+
+    Benjamim.JWT.Header.Algorithm(TJwtAlgorithm.HS256); { OPTIONAL - DEFAULT TJwtAlgorithm.HS256 }
+
+    Benjamim.JWT.Payload
+      .jti(1)
+      .iss('Luis Nt')
+      .sub('Chave de acesso')
+      .aud('192.168.0.77')
+      .iat('2021-01-31 15:55:21.123')
+      .nbf('2021-01-31 18:01:01.001')
+      .exp('2021-01-31 22:01:01.001')
+      .add('price', 10.5)
+      .add('name', 'your fullname')
+      .add('phone', 559822223333)
+      ;
+    LToken := Bemjamim.JWT.Signature.Sign;
+
+    WriteLn(LToken);
 
   except
     on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+      writeln(E.ClassName, ': ', E.Message);
   end;
 
 end.
